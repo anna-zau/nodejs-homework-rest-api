cd7b4ae150c3287cb045a7f4f17c3452ca200b79
anna-zau <104685927+anna-zau@users.noreply.github.com>
2023-03-03 22:33:11 +0200

    Update validateBody.js

--- middlewars/validateBody.js
+++ middlewars/validateBody.js
@@ -1,13 +1,14 @@
-const HttpsError = require("../helpers");
+const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   const fnc = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpsError(404, error.message));
+      next(HttpError(400, error.message));
     }
     next();
   };
+
   return fnc;
 };
 
